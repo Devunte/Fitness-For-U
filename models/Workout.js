c003c7a75f6919417c920050305fa0fd487fb01f
DJ <demetriuswhitejr.@Demetriuss-MBP.lan>
2024-05-09 11:22:23 -0500

    User Model and Workout model

---
+++ models/Workout.js
@@ -0,0 +1,48 @@
+const { model, DataTypes } = require('sequilize')
+const sequelize = require('../config/connection')
+class Workout extends model{}
+
+Workout.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+      },
+      date_created: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id',
+        },
+      },
+      exercise_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'exercise',
+            key: 'id'
+        }
+      },
+    },
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'workout',
+      },
+);
+module.exports = Workout;
