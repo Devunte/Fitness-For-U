616c22672ea87c32b4f401c519f39383fb64da7d
joelamaya19 <joelamaya99@gmail.com>
2024-05-13 10:37:52 -0500

    added icon for exercise model

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,24 +7,24 @@ router.get('/', async (req, res) => {
             include: [{ model: Workout }]
         });
 
-        const exercises = exerciseData.map((exercise) => exercise.get({plain: true}));
+        const exercises = exerciseData.map((exercise) => exercise.get({ plain: true }));
 
         res.render('homepage', {
             exercises,
-            logged_in: req.session.logged_in 
+            logged_in: req.session.logged_in
         })
-        } catch (err) {
-                res.status(500).json(err);
-            }
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
-router.get('/exercise/:id', async (req,res) => {
+router.get('/exercise/:id', async (req, res) => {
     try {
         const exerciseData = await Exercise.findByPk(req.params.id, {
-            include: [{model: Workout}]
+            include: [{ model: Workout }]
         });
         const exercise = exerciseData.get({ plain: true });
-    
+
         res.render('exercise', {
             ...exercise,
         });
@@ -33,17 +33,17 @@ router.get('/exercise/:id', async (req,res) => {
     }
 });
 
-router.get('/workout/:id', async (req,res) => {
-    try{
+router.get('/workout/:id', async (req, res) => {
+    try {
         const workoutData = await Workout.findByPk(req.params.id, {
-            
+
         });
         const workout = workoutData.get({ plain: true });
 
-        res.render('workout',{
+        res.render('workout', {
             ...workout,
         });
-    } catch(err) {
+    } catch (err) {
         res.status(500).json(err);
     }
 });
@@ -51,11 +51,11 @@ router.get('/workout/:id', async (req,res) => {
 router.get('/login', (req, res) => {
     // If the user is already logged in, redirect the request to another route
     if (req.session.logged_in) {
-      res.redirect('/homepage');
-      return;
+        res.redirect('/homepage');
+        return;
     }
-  
+
     res.render('login');
-  });
+});
 
 module.exports = router;
